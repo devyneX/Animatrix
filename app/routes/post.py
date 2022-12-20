e6908c83a56a0369e2850b4bdb8225fd57e41379
devyneX <ahmed.symum.swapno@g.bracu.ac.bd>
2022-12-20 13:23:26 +0600

    post page corner case resolved

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from ..controllers.post_controller import PostController
 
-post = Blueprint("post", __name__, template_folder="app/views/post")
+post = Blueprint("post", __name__, template_folder="../views/post")
 controller = PostController()
 
 post.route("/<id>", methods=["GET"])(controller.post)
